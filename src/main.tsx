5a361ddd3f90af30a168cc81262e42b72a394556
Developer <dev@kerberlabs.com>
2026-01-20 15:43:19 -0300

    Force HTTPS redirect on production

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Force HTTPS on production (not on localhost)
+if (window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1' && window.location.protocol === 'http:') {
+  window.location.protocol = 'https:';
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
